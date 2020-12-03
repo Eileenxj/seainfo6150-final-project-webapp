588f1c15cd3d870dbbaad4ad2afc72de3c4c8915
JUN <jun@JUNdeMacBook-Pro.local>
2020-12-03 14:56:09 +0800

    Updates

---
+++ precache-manifest.d7997c74c01430f25e50d4c6121696fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3ac123f6ac739449d2ea74b94f3d91",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "09699fa1098d2dd41740",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.10d31c33.chunk.js"
+  },
+  {
+    "revision": "e640d77a187a88d71b83",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.3dfacf66.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  }
+]);
